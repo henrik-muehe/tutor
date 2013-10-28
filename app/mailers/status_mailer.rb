289e510a5bd2248c7e973a7da8fda831ae26db5b
Henrik Muehe <muehe@in.tum.de>
2013-10-28 16:02:59 +0100

    changed reply email address

--- app/mailers/status_mailer.rb
+++ app/mailers/status_mailer.rb
@@ -1,5 +1,5 @@
 class StatusMailer < ActionMailer::Base
-  default from: "Tutor Tool <kemperwebtools@gmail.com>"
+  default from: "Tutor Tool <no-reply@db.in.tum.de>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
