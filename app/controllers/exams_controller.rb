cdd950664b23b379f7c4aafb28a3d992306f4529
Henrik Mühe <muehe@in.tum.de>
2014-02-25 09:59:59 +0100

    debugging

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -260,6 +260,6 @@ class ExamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def exam_params
-      params.require(:exam).permit(:name, :start, :original_import, :magictoken, { :room_ids => [] }, :exam_tasks_attributes => [:id, :number, :name, :max_points, :_destroy] )
+      params.require(:exam).permit(:name, :start, :course_id, :original_import, :magictoken, { :room_ids => [] }, :exam_tasks_attributes => [:id, :number, :name, :max_points, :_destroy] )
     end
 end
